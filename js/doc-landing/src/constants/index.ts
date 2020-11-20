fa6190a71930b66a8dd56dbd89d9578ad878f398
Benjamin Blazke <benjamin.blazke@coursehero.com>
2020-11-20 09:48:00 -0800

    port 5005

--- js/doc-landing/src/constants/index.ts
+++ js/doc-landing/src/constants/index.ts
@@ -1,5 +1,5 @@
 // Base URL for the server
-export const BASE_URL = 'http://127.0.0.1:5000'
+export const BASE_URL = 'http://127.0.0.1:5005'
 
 // The default doc rendered when there isn't a matching course in the route
 export const DEFAULT_DOC = 'BIO-101'
