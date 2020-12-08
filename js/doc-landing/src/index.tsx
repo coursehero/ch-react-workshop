f67463498f918224a17a4ee11afd16acf6f6f1a1
Ernesto Rodriguez <ernesto.rodriguez@coursehero.com>
2020-12-08 15:01:12 -0800

    Add browser mock defaults

--- js/doc-landing/src/index.tsx
+++ js/doc-landing/src/index.tsx
@@ -9,6 +9,12 @@ import './index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
+// if (process.env.NODE_ENV === 'development') {
+//   const { worker } = require('./test-utils/browser')
+
+//   worker.start()
+// }
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
