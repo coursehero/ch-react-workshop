14dab9560cdc6a2993f641582af3bcc7d4c569b1
davidkhayutin <david.khayutin@gmail.com>
2022-07-25 15:23:55 -0700

    remove test assertions

--- js/doc-landing/src/components/DocLanding/DocLanding.test.tsx
+++ js/doc-landing/src/components/DocLanding/DocLanding.test.tsx
@@ -48,73 +48,9 @@ describe('DocLanding', () => {
     jest.clearAllMocks()
   })
 
-  test('Renders the document correct on screen', async () => {
-    setUpFetch()
-    renderComponent()
+  test('Renders the document correct on screen', async () => {})
 
-    expect(mockFetch).toBeCalledTimes(1)
-    expect(screen.getByTestId('doc-landing-spinner')).toBeInTheDocument()
+  test('Changes the window to correct URL when button is clicked', async () => {})
 
-    await waitForElementToBeRemoved(() =>
-      screen.getByTestId('doc-landing-spinner'),
-    )
-
-    // asserts the logo on screen with the correct parent anchor
-    expect(screen.getByRole('link', { name: 'logo' })).toHaveAttribute(
-      'href',
-      'http://127.0.0.1:5005/',
-    )
-
-    // asserting by aria label and find listItems within (breadcrumb length)
-    // expect(
-    //   within(screen.getByLabelText('breadcrumb')).getAllByRole('listitem'),
-    // ).toHaveLength(3)
-
-    // expect(
-    //   screen.getByTestId(`doc-title-${MOCK_DOCS[DEFAULT_DOC].name}`),
-    // ).toBeInTheDocument()
-
-    // expect(
-    //   screen.getByTestId(`doc-school-${MOCK_DOCS[DEFAULT_DOC].school}`),
-    // ).toBeInTheDocument()
-
-    // expect(screen.getByText(MOCK_DOCS[DEFAULT_DOC].text)).toBeInTheDocument()
-
-    // expect(
-    //   screen.getByRole('button', { name: 'Unlock Document' }),
-    // ).toBeInTheDocument()
-  })
-
-  test('Changes the window to correct URL when button is clicked', async () => {
-    setUpFetch()
-    renderComponent()
-
-    // expect(mockFetch).toBeCalledTimes(1)
-    // expect(screen.getByTestId('doc-landing-spinner')).toBeInTheDocument()
-
-    // await waitForElementToBeRemoved(() =>
-    //   screen.getByTestId('doc-landing-spinner'),
-    // )
-    // expect(window.location.href).toEqual(mockUrl)
-
-    // const button = screen.getByRole('button', { name: 'Unlock Document' })
-    // fireEvent.click(button)
-    // expect(window.location.href).toEqual(redirectUrl)
-  })
-
-  test('Asserts error message when an error is returned from the network request', async () => {
-    setUpFetch(false)
-    // Hides Error for this tests since our code always adds a log
-    // Not the best practice but leaves a cleaner looking tests suite
-    // jest.spyOn(console, 'error').mockImplementation()
-    // renderComponent()
-
-    // expect(mockFetch).toBeCalledTimes(1)
-
-    // await waitForElementToBeRemoved(() =>
-    //   screen.getByTestId('doc-landing-spinner'),
-    // )
-
-    // expect(screen.getByText('Something went wrong...')).toBeInTheDocument()
-  })
+  test('Asserts error message when an error is returned from the network request', async () => {})
 })
--- js/doc-landing/src/components/RelatedDocs/RelatedDocs.test.tsx
+++ js/doc-landing/src/components/RelatedDocs/RelatedDocs.test.tsx
@@ -14,56 +14,21 @@ describe('Related Docs Test', () => {
     beforeEach(() => {
       setup({})
     })
-    it('renders on screen properly', async () => {
-      // validates heading
-      expect(
-        screen.getByRole('heading', {
-          name: 'Students who viewed this also studied',
-        }),
-      ).toBeInTheDocument()
 
-      // validates list
-      expect(screen.getAllByRole('listitem')).toHaveLength(3)
-
-      // validates each list item
-      MOCK_RELATED_DOCS.forEach((doc: string) => {
-        expect(screen.getByTestId(`related-doc-${doc}`)).toBeInTheDocument()
-        expect(screen.getByRole('link', { name: doc })).toBeInTheDocument()
-      })
-    })
+    it('renders on screen properly', async () => {})
 
     /*
     / ALTERNATE APPROACH - Discuss Schools of thought behind both
     */
-    it('renders the title on screen properly', async () => {
-      expect(
-        screen.getByRole('heading', {
-          name: 'Students who viewed this also studied',
-        }),
-      ).toBeInTheDocument()
-    })
-    it('renders the ordered list on screen properly', async () => {
-      expect(screen.getAllByRole('listitem')).toHaveLength(3)
-    })
 
-    it('renders each item in the list', async () => {
-      MOCK_RELATED_DOCS.forEach((doc: string) => {
-        expect(screen.getByTestId(`related-doc-${doc}`)).toBeInTheDocument()
-      })
-    })
+    it('renders the title on screen properly', async () => {})
+    it('renders the ordered list on screen properly', async () => {})
+    it('renders each item in the list', async () => {})
   })
 
   // Assertions for elements that are no longer there
 
   describe('When no related documents are provided', () => {
-    it('does not render a list when no related docs are supplied', () => {
-      setup({ related: [] })
-      // All Three Do the same thing! So how do we decide which is the best practice
-      expect(screen.getByRole('list')).not.toContain('ChildComponent')
-      expect(screen.queryByRole('listitem')).toBeNull()
-      MOCK_RELATED_DOCS.forEach((doc: string) => {
-        expect(screen.queryByTestId(`related-doc-${doc}`)).toBeNull()
-      })
-    })
+    it('does not render a list when no related docs are supplied', () => {})
   })
 })
